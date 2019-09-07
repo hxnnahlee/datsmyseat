13725c16dc4b567a293e15b76387fd0303f007af
Angel Chen <anc520@lehigh.edu>
2019-09-07 19:38:05 -0400

    backend.py

--- backend.py
+++ backend.py
@@ -88,77 +88,75 @@ def index():
 ## BaseUrl for Android http://<your ip address>:5000/spots/<spots>
 
 # POST route for sensor #1
-@app.route('/spots/1', methods = ['POST'])
-def spot_taken():
-    threadLock.acquire()
+with threadLock:
+    @app.route('/spots/1', methods = ['POST'])
+    def spot_taken():
+        currentDistance = int(request.data.decode("utf-8"))
+        #sensorOneCount = get_sensorOneCount()
+        sensorOneState = get_sensorOneState()
+        global sensorOneCount 
 
-    currentDistance = int(request.data.decode("utf-8"))
-    #sensorOneCount = get_sensorOneCount()
-    sensorOneState = get_sensorOneState()
-    global sensorOneCount 
+        #print("State: " + str(get_sensorOneState()))
 
-    #print("State: " + str(get_sensorOneState()))
+        print(sensorOneCount)
+        sensorOneCount = sensorOneCount + 1
 
-    print(sensorOneCount)
-    sensorOneCount = sensorOneCount + 1
+        set_sensorOneState(sensorOneState + 1)
 
-    set_sensorOneState(sensorOneState + 1)
+        return str(get_sensorOneState())
 
-    threadLock.release()
-    return str(get_sensorOneState())
-
-
-"""
-    # Person is currently not in the seat
-    if int(sensorOneState) == 0:
-        if currentDistance < 300:
-            set_sensorOneState(1)
-            sensorOne[sensorOneCount] = currentDistance
-            set_sensorOneCount(sensorOneCount + 1)
-
-    # Person might not be in the seat
-    elif int(sensorOneState) == 1:
-
-        # We recorded 20 calls, time to make a state transition decision
-        if sensorOneCount == 20:
-            sensorOne.sort()
-            medianDistance = sensorOne[10]
-
-            # Person probably walked pass the sensor
-            if medianDistance > 300:
-                set_sensorOneState(0)
-                set_sensorOneCount(0)
-
-            # There is a person in the seat now
-            else:
-                set_sensorOneState(2)
-                set_sensorOneCount(0)
-
-        else:
-            sensorOne[sensorOneCount] = currentDistance
-            set_sensorOneCount(sensorOneCount + 1)
-
-    # Person is in the seat 
-    elif sensorOneState == 2:
-        if currentDistance > 300:
-            set_sensorOneState(3)
-            sensorOne[sensorOneCount] = currentDistance
-            set_sensorOneCount(sensorOneCount + 1)
-
-    # Person might be in the seat
-    elif sensorOneState == 3:
-
-        # We recorded 20 calls, time to make a state transition decision
-        if sensorOneCount == 20:
-            if medianDistance < 300:
-                set_sensorOneState(2)
-                set_sensorOneCount(0)
-
-            else:
-                set_sensorOneState(0)
-                set_sensorOneCount(0)
-
-    print("Count: " + str(get_sensorOneCount()))
 
     """
-    #return str(get_sensorOneState())
+        # Person is currently not in the seat
+        if int(sensorOneState) == 0:
+            if currentDistance < 300:
+                set_sensorOneState(1)
+                sensorOne[sensorOneCount] = currentDistance
+                set_sensorOneCount(sensorOneCount + 1)
+
+        # Person might not be in the seat
+        elif int(sensorOneState) == 1:
+
+            # We recorded 20 calls, time to make a state transition decision
+            if sensorOneCount == 20:
+                sensorOne.sort()
+                medianDistance = sensorOne[10]
+
+                # Person probably walked pass the sensor
+                if medianDistance > 300:
+                    set_sensorOneState(0)
+                    set_sensorOneCount(0)
+
+                # There is a person in the seat now
+                else:
+                    set_sensorOneState(2)
+                    set_sensorOneCount(0)
+
+            else:
+                sensorOne[sensorOneCount] = currentDistance
+                set_sensorOneCount(sensorOneCount + 1)
+
+        # Person is in the seat 
+        elif sensorOneState == 2:
+            if currentDistance > 300:
+                set_sensorOneState(3)
+                sensorOne[sensorOneCount] = currentDistance
+                set_sensorOneCount(sensorOneCount + 1)
+
+        # Person might be in the seat
+        elif sensorOneState == 3:
+
+            # We recorded 20 calls, time to make a state transition decision
+            if sensorOneCount == 20:
+                if medianDistance < 300:
+                    set_sensorOneState(2)
+                    set_sensorOneCount(0)
+
+                else:
+                    set_sensorOneState(0)
+                    set_sensorOneCount(0)
+
+        print("Count: " + str(get_sensorOneCount()))
+
+        """
+        #return str(get_sensorOneState())
