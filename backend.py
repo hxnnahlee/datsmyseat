740c79ded5a277eb5c4982fb5f6fd38b7715ea01
Angel Chen <anc520@lehigh.edu>
2019-09-07 19:48:27 -0400

    atomic

--- backend.py
+++ backend.py
@@ -3,6 +3,51 @@ from array import array
 from flask import g
 import threading
 
+class AtomicCounter:
+    """An atomic, thread-safe incrementing counter.
+    >>> counter = AtomicCounter()
+    >>> counter.increment()
+    1
+    >>> counter.increment(4)
+    5
+    >>> counter = AtomicCounter(42.5)
+    >>> counter.value
+    42.5
+    >>> counter.increment(0.5)
+    43.0
+    >>> counter = AtomicCounter()
+    >>> def incrementor():
+    ...     for i in range(100000):
+    ...         counter.increment()
+    >>> threads = []
+    >>> for i in range(4):
+    ...     thread = threading.Thread(target=incrementor)
+    ...     thread.start()
+    ...     threads.append(thread)
+    >>> for thread in threads:
+    ...     thread.join()
+    >>> counter.value
+    400000
+    """
+    def __init__(self, initial=0):
+        """Initialize a new atomic counter to given initial value (default 0)."""
+        self.value = initial
+        self._lock = threading.Lock()
+
+    def increment(self, num=1):
+        """Atomically increment the counter by num (default 1) and return the
+        new value.
+        """
+        with self._lock:
+            self.value += num
+            return self.value
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+    
 app = Flask(__name__)
 # define some arrays to store distances for each sensor
 sensorOne  = array('q')
@@ -157,46 +202,3 @@ def spot_taken():
     #return str(get_sensorOneState())
 
 
-class AtomicCounter:
-    """An atomic, thread-safe incrementing counter.
-    >>> counter = AtomicCounter()
-    >>> counter.increment()
-    1
-    >>> counter.increment(4)
-    5
-    >>> counter = AtomicCounter(42.5)
-    >>> counter.value
-    42.5
-    >>> counter.increment(0.5)
-    43.0
-    >>> counter = AtomicCounter()
-    >>> def incrementor():
-    ...     for i in range(100000):
-    ...         counter.increment()
-    >>> threads = []
-    >>> for i in range(4):
-    ...     thread = threading.Thread(target=incrementor)
-    ...     thread.start()
-    ...     threads.append(thread)
-    >>> for thread in threads:
-    ...     thread.join()
-    >>> counter.value
-    400000
-    """
-    def __init__(self, initial=0):
-        """Initialize a new atomic counter to given initial value (default 0)."""
-        self.value = initial
-        self._lock = threading.Lock()
-
-    def increment(self, num=1):
-        """Atomically increment the counter by num (default 1) and return the
-        new value.
-        """
-        with self._lock:
-            self.value += num
-            return self.value
-
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
