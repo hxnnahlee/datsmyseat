daaf0df5ad0c902e49aa935631e58b1d94fdc98d
Hannah Lee <hannahl1409@gmail.com>
2019-09-07 14:42:16 -0400

    Added Procfile

---
+++ backend.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+
+# root
+@app.route("/")
+def index():
+    """
+    this is a root dir of my server
+    :return: str
+    """
+    return "This is root!!!!"
+
+# GET
+@app.route('/spots/<spot>', methods = ['POST'])
+def spot_taken(spot):
+    print(request.data)
+
+    return request.data
+
+#if __name__ == "__main__":
+#    app.run()
+#testing purposes
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+## server on http://0.0.0.0:5000/
+## visible across the network
+## BaseUrl for Android http://<your ip address>:5000/spots/<spots>
+
